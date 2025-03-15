fa5cf8dbb22def15e74a73578fc70c865597412e
tehoro <tehoro@users.noreply.github.com>
2025-03-15 13:16:39 +1300

    Update imageprocessor.py
    Fixed file name issue in csv file write

--- imageprocessor.py
+++ imageprocessor.py
@@ -209,14 +209,13 @@ class ImageProcessorThread(QThread):
     
     def write_exif_csv(self, exif_csv_path, exif_data_list):
         """Write EXIF data to CSV file"""
-        with open(exif_csv_path, 'w', newline='') as csvfile:
+        with open(exif_csv_path, 'w', newline='', encoding='utf-8') as csvfile:
             fieldnames = ['FileName', 'OriginalFileName', 'Width', 'Height', 
                          'DateTimeCreated', 'DateTimeOriginal']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for data in exif_data_list:
                 writer.writerow(data)
-
 
 class PSNZImageProcessor(QMainWindow):
     def __init__(self):
